0cc6983f09096683eefe7282ac8539a8bf90db57
aplicaciones-sed <aplicacionessed@narino.gov.co>
2026-03-17 18:05:55 -0500

    primera version todas asignaturas

--- sw.js
+++ sw.js
@@ -1,32 +1,15 @@
-const CACHE_NAME = 'geoquiz-v1';
-const ASSETS = [
-  './quiz-matematicas.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-// Instalar: guarda archivos en caché
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+const CACHE = 'saber11-narino-v1';
+const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-// Activar: limpia cachés viejos
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
-
-// Fetch: responde con caché, si falla va a la red
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
